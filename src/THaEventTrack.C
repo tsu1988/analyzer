52e9239a8fb253ae9f19d18dfb10dac3d1370a12
stringer <stringer>
2003-06-26 20:42:12 +0000

    Class to display events grapically in detector stack. Currently only draws detectors and VDC Track reconstruction.

---
+++ src/THaEventTrack.C
@@ -0,0 +1,238 @@
+//*-- Author : 	Robert Stringer	17-Jun-03
+//
+//////////////////////////////////////////////////////
+//
+// THaEventTrack
+//
+// Physics Module to display single/multi track events 
+// through the detector stack.
+//
+/////////////////////////////////////////////////////
+
+#include "THaEventTrack.h"
+#include "TVector3.h"
+#include "THaSpectrometerDetector.h"
+#include <math.h>
+#include "TBox.h"
+#include "THaTrack.h"
+#include "TRotMatrix.h"
+#include "TBRIK.h"
+#include "TNode.h"
+
+//----------------------------------------------------
+THaEventTrack::THaEventTrack(const char* name, THaApparatus* app): THaPhysicsModule(name,"EventTrack")
+{
+  printf("In Const");
+  if(!app || !app->InheritsFrom("THaSpectrometer")) //Do not allow null apparatus.
+  {
+    printf("Not a spec");
+    Error( Here("THaEventTrack()"), "Apparatus %s is not a THaSpectrometer.",
+	app->GetName());
+    fApp=NULL;
+  }
+  else
+  {
+    fApp = app;
+    SetCentralAngle(TVector3(1,1,1));
+  }
+}
+
+//----------------------------------------------------
+THaEventTrack::~THaEventTrack()
+{
+  // Destructor
+	
+
+  // Free Graphics
+	
+  TIter itr(&fGraphics);
+  TObject *t;
+  while(t = itr.Next())
+  {
+    printf("Freed.\n");
+    delete t;
+  }
+}
+//----------------------------------------------------
+Int_t THaEventTrack::Process( const THaEvData& evdata )
+{
+  // do something ...
+ 
+  //Test 
+ /* 
+    if( !fIsSetup ) 
+    {
+	fTest = new THaCut( "_Test", "" , "_Block" );
+	// Expression error?
+	if( !fTest || fTest->IsZombie()) 
+	{
+	  delete fTest; fTest = NULL;
+        }
+	fIsSetup = true;
+    }
+    bool good = true;
+    if ( fTest && !fTest->EvalCut())
+	    return 0;
+                             
+*/
+	
+  Int_t ntracks = fSpectro->GetNTracks();
+  if( ntracks == 0 )
+	  return 0;
+//test-look at only multi-track events.
+//  if( ntracks == 1 )
+//	  return 0;
+  
+  TClonesArray* tracks = fSpectro->GetTracks();
+  if (!tracks)
+	  return -2;
+  
+  //Clear old hits
+
+  TList* nodes = (fGeom->GetCurrentNode())->GetListOfNodes();
+  TIter next(nodes);
+  TNode* obj;
+  
+  while( obj = (TNode*) next())
+  {  
+
+//   cout << "Node:" << obj->GetName() << obj <<"\n";
+    if(TString(obj->GetName()).Contains("HIT"))
+    {
+	    fGeom->RecursiveRemove(obj);
+    }
+  }
+
+   
+  Int_t ndetectors = fApp->GetNumDets();
+
+  for(Int_t i = 0; i < ntracks; i++)
+  { 
+    THaTrack* theTrack = static_cast<THaTrack*>( tracks->At(i));
+    if(!theTrack || ! theTrack->HasTarget() )
+    	continue;
+    for(Int_t i=0 ;i < ndetectors;i++)
+    {
+      THaDetector* dobj =  fApp->GetDetector(i);
+      Double_t t;
+      if(TString(dobj->ClassName()) == "THaScintillator") 
+      {
+	dobj->Draw(fGeom,evdata);
+      }
+      else
+      {      
+        dobj->Draw(fGeom,theTrack,t);
+      }
+    }
+			  
+  }
+
+  fGeom->Draw();
+  fCanvas->Update();
+
+  // Wait for user input
+  cout << "RETURN: continue, H: run 100 events, R: run to end, F: finish quietly, Q: quit\n";
+   char c;
+   cin.clear();
+  while( !cin.eof() && cin.get(c) && !strchr("\nqQhHrRfF",c));
+  if( c != '\n' ) while( !cin.eof() && cin.get() != '\n');
+    if( tolower(c) == 'q' )
+      return kTerminate;
+    else if( tolower(c) == 'h' ) 
+    {
+       fFlags |= kCount;
+       fFlags &= ~kStop;
+       fCount = 100;
+    }
+    else if( tolower(c) == 'r' )
+    fFlags &= ~kStop;
+  else if( tolower(c) == 'f' ) 
+  {
+    fFlags &= ~kStop;
+    fFlags |= kQuiet;
+  }
+                                                                             
+  return 0;
+}
+
+//----------------------------------------------------
+THaAnalysisObject::EStatus THaEventTrack::Init( const TDatime& run_time)
+{
+  // Initialize the module.
+
+  if( THaPhysicsModule::Init( run_time ))
+  {
+    return fStatus;
+  }
+  if(!fApp)
+  {
+	  return kInitError;
+  }
+  //Initialize the Canvas
+
+  TRotMatrix* zero = NULL;
+  fCanvas = new TCanvas("EventTrack");
+  fGeom = new TGeometry("EventTrack","EventTrack");
+  fGeom->GetListOfMatrices()->Add(new TRotMatrix("XY","XY",45,0,90,90,-45,0));
+  zero = new TRotMatrix("ZERO","ZERO",90,0,90,90,0,0);
+  fGeom->GetListOfMatrices()->Add(zero);
+
+  fGeom->SetMatrix(zero);
+  new TBRIK("DUM","DUM","void",0,0,0);
+  fGeom->Node("DUMMY","Center","DUM" ,0,0,0);
+  
+  // Get the spectrometer
+  fSpectro = static_cast<THaSpectrometer*>
+	  ( FindModule( fApp->GetName(), "THaSpectrometer" ));
+  
+  if(!fSpectro )
+	  return kInitError;
+
+  //Find the included detectors.
+  
+  
+  Int_t ndetectors = fApp->GetNumDets();
+
+  TString buf(3);
+  for(Int_t i= ndetectors-1 ;i >= 0;i--)
+  {
+    THaDetector* dobj =  fApp->GetDetector(i);
+  
+    buf="";
+    buf+=i+1;
+
+    dobj->Draw(fGeom,buf);
+	
+  }
+ 
+  fGeom->Draw();
+
+  return kOK;
+}
+//----------------------------------------------------
+Int_t THaEventTrack::DrawDetector(const char* name, Float_t sx, Float_t sy, Float_t d)
+{
+  //Draws the detectors on the canvas.
+
+  Double_t x1 = (Double_t) d * fCentralAngle.CosTheta() - .5*sx;
+  Double_t y1 = (Double_t) d * sin(fCentralAngle.Theta()) - .5*sy;
+  Double_t x2 = (Double_t) d * fCentralAngle.CosTheta() + .5*sx;
+  Double_t y2 = (Double_t) d * sin(fCentralAngle.Theta()) + .5*sy;
+  	
+  printf("%s: cos-angle = %f, dist = %f",name,fCentralAngle.CosTheta(),d);
+  TBox* b = new TBox(x1/4,y1/4,x2/4,y2/4);
+
+  fGraphics.Add(b);
+  
+  b->SetFillColor(0);
+  b->SetFillStyle(0);
+  b->Draw();
+
+  return kOK;
+}
+
+//----------------------------------------------------
+
+//----------------------------------------------------
+
+ClassImp(THaEventTrack)
---
+++ src/THaEventTrack.h
@@ -0,0 +1,69 @@
+#ifndef ROOT_THaEventTrack
+#define ROOT_THaEventTrack
+
+//////////////////////////////////////////////////////////////////////
+//
+// THaEventTrack
+//
+//////////////////////////////////////////////////////////////////////
+
+
+#include "THaPhysicsModule.h"
+#include "THaApparatus.h"
+#include "TCanvas.h"
+#include "TVector3.h"
+#include "TGeometry.h"
+#include "THaSpectrometer.h"
+#include "THaCut.h"
+
+class THaCut;
+
+class THaEventTrack : public THaPhysicsModule {
+
+public:
+  THaEventTrack(const char* name,  THaApparatus* app);
+  virtual ~THaEventTrack();
+
+  virtual EStatus 	Init( const TDatime& run_time );
+  virtual Int_t		Process( const THaEvData& evdata);
+
+  	  void		SetCentralAngle(TVector3 cangle);
+	  
+protected:
+	
+	THaSpectrometer* fSpectro;
+  	THaApparatus* 	fApp;
+  	TCanvas*	fCanvas;
+	THaCut*		fTest;
+	
+	Int_t		fFlags;
+	Int_t		fCount;
+	bool		fIsSetup;
+	
+  	TVector3	fCentralAngle;
+
+  	Int_t		DrawDetector(const char* name,Float_t sx,Float_t sy,Float_t d);
+  	TList		fGraphics;
+ 	TGeometry*	fGeom; 
+	  
+	enum EFlags {
+	      kStop  = BIT(0),    // Wait for key press after every event
+	      kCount = BIT(1),    // Run for fCount events
+	      kQuiet = BIT(2)     // Run quietly (don't print variables)
+	};
+
+private:
+
+  
+//  THaEventTrack();
+//  THaEventTrack( const THaEventTrack& );
+//  THaEventTrack& operator=( const THaEventTrack& );
+
+  ClassDef(THaEventTrack,0)  
+};
+inline void THaEventTrack::SetCentralAngle(TVector3 cangle)
+{
+	fCentralAngle = cangle;
+}
+
+#endif
