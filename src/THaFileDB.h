216c66f6f4342832edabb8e557fa390198d28fce
Ole Hansen <ole@jlab.org>
2013-04-05 01:30:06 -0400

    Implement THaFileDB data structures and LookupKey().

--- src/THaFileDB.h
+++ src/THaFileDB.h
@@ -10,23 +10,49 @@
 //////////////////////////////////////////////////////////////////////////
 
 #include "THaDB.h"
-//#include <cstdio>
+#include <map>
+#include <set>
+#include <cstdio>
+#include <ctime>
 
 class THaFileDB : public THaDB {
  public:
   THaFileDB( const char* DB_DIR = 0 );
   virtual ~THaFileDB();
   
+  // Prevent hiding of the overloaded LoadValue instances
   using THaDB::LoadValue;
 
-  //  LoadFile( );
-
   virtual Int_t Close();
-
   virtual Int_t LoadValue( const char* key, std::string& text,
 			   const TDatime& date = TDatime() );
 
+  Int_t LookupKey( const char* key, std::string& text, time_t timestamp );
+
+  // Support functions for THaAnalyzer::LoadDB interface.
+  // Will be deprecated in a future release.
+
+  Int_t LoadFile( FILE* file, const TDatime& date );
+
  protected:
+
+
+  struct Value_t {
+    UInt_t       range_id;
+    std::string  value;
+  };
+  struct TimeRange_t {
+    time_t       start_time;
+    time_t       end_time;
+  };
+
+  typedef std::map<std::string,UInt_t>   StringMap_t;
+  typedef std::multimap<UInt_t,Value_t>  ValueMap_t;
+  typedef std::map<UInt_t,TimeRange_t>   TimeRangeMap_t;
+
+  StringMap_t    fKeys;         // Unique keys -> key IDs (1-to-1)
+  ValueMap_t     fValues;       // key IDs -> ranges/values (1-to-many)
+  TimeRangeMap_t fTimeRanges;   // Range IDs -> time periods (1-to-1)
 
   ClassDef(THaFileDB,0)   // Hall A-style file-based database backend
 };
